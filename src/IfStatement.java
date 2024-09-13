ef98dc1bbe04f76dcc0284589991b08dbad46d06
Marco <marco.bischof@student.htldornbirn.at>
2024-09-13 13:05:31 +0200

    IfStatement

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -1,5 +1,3 @@
-package at.ran.basics;
-
 import java.util.Random;
 
 
@@ -14,10 +12,21 @@ public class IfStatement {
         Random random = new Random();
         int randomNumber = random.nextInt(100);
 
-        if(randomNumber < 20) {
+        // Gib die Zufallszahl aus
+        System.out.println(randomNumber);
 
+
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+        if (randomNumber < 20) {
+            System.out.println("Size: Mini");
+
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
         } else if (randomNumber > 20 && randomNumber < 50) {
+            System.out.println("Size: Medium");
 
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+        } else if (randomNumber > 50) {
+            System.out.println("Size: large");
         }
 
 
@@ -25,3 +34,8 @@ public class IfStatement {
 
     }
 }
+
+
+
+
+
