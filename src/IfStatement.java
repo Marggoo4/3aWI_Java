b3b6b92f1d71920bbb6eb15df19e0a50d85e7272
Marco <marco.bischof@student.htldornbirn.at>
2024-09-13 12:56:06 +0200

    Test

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -1,7 +1,27 @@
+package at.ran.basics;
+
+import java.util.Random;
+
+
+
+
 public class IfStatement {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        if(randomNumber < 20) {
+
+        } else if (randomNumber > 20 && randomNumber < 50) {
+
+        }
+
+
+
+
     }
 }
-
-
