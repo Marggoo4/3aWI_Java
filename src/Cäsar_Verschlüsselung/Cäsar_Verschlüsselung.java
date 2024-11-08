0b185d077ce8b08154b79519da888b97ecd48bdc
Marco <marco.bischof@student.htldornbirn.at>
2024-11-08 11:51:39 +0100

    Cäsar_Verschlüsselung_Finished

--- src/Cäsar_Verschlüsselung/Cäsar_Verschlüsselung.java
+++ src/Cäsar_Verschlüsselung/Cäsar_Verschlüsselung.java
@@ -1,22 +1,29 @@
 package Cäsar_Verschlüsselung;
 
+import java.util.Scanner;
+
 public class Cäsar_Verschlüsselung {
     public static void main(String[] args) {
-        String result = encrypt("abc");
-        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Text: ");
+        // Benutzer gibt den Text ein
 
-        String decrypted = decrypt(result);
-        System.out.println(decrypted);
+        String s = scanner.nextLine();
+        int offset = 2;
 
+        String encrypted = encrypt(s, offset);
+        System.out.println(" " + encrypted);
     }
 
-    public static String encrypt(String data) {
-        StringBuilder result = new StringBuilder();
-
+    public static String encrypt(String s, int offset) {
         String result = "";
         for (char c : s.toCharArray()) {
             if (c >= 'a' && c <= 'z') {
+                result += (char) (((c - 'a' + offset) % 26) + 'a');
+
+            } else if (c >= 'A' && c <= 'Z') {
+                result += (char) (((c - 'A' + offset) % 26) + 'A');
 
             } else {
                 result += c;
@@ -24,19 +31,8 @@ public class Cäsar_Verschlüsselung {
         }
         return result;
     }
-
-
-    public static String decrypt(String data) {
-
-    }
-
 }
+
 
 // Bei der Cäsar-Verschlüsselung werden die Buchstaben um eine fixe Anzahl Stellen verschoben (z.B. 2) a wird zu c, f wird zu h, z wird zu b usw.
 // Implementiere eine Methode encrypt(String):String und eine Methode decrypt(String):String
-
-
-
-
-
-
